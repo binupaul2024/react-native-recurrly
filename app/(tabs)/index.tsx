ab8c675398a630dc27061fabd940269adc057f1d
Binu Augustine <binua77@gmail.com>
2026-05-22 14:18:42 +0200

    impelents fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text } from "react-native";
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
@@ -9,43 +9,41 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white px-4 py-2"
+        className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"
       >
         <Text className="text-white">Go to Onboarding</Text>
       </Link>
 
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white px-4 py-2"
+        className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"
       >
         <Text className="text-white">Sign In</Text>
       </Link>
 
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white px-4 py-2"
+        className="mt-4  font-sans-bold rounded bg-primary text-white px-4 py-2"
       >
         <Text className="text-white">Sign Up</Text>
       </Link>
 
-      <Link
+      {/* <Link
         href="/subscriptions/spotify"
-        className="mt-4 rounded bg-primary text-white px-4 py-2"
+        className="mt-4  font-sans-bold rounded bg-primary text-white px-4 py-2"
       >
         Spotify Subscription
       </Link>
 
       <Link
-        className="mt-4 rounded bg-primary text-white px-4 py-2"
+        className="mt-4  font-sans-bold rounded bg-primary text-white px-4 py-2"
         href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
       >
         Claude Subscription
-      </Link>
+      </Link> */}
     </SafeAreaView>
   );
 }
